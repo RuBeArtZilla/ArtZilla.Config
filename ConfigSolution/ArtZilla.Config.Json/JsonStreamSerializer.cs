3dc0c9da331fd80f262ac66b0eabc338e350b61c
ArtZilla <mail@artzilla.name>
2020-06-27 10:32:35 +0600

    add json package

---
+++ ConfigSolution/ArtZilla.Config.Json/JsonStreamSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using ArtZilla.Config.Configurators;
+using Newtonsoft.Json;
+
+namespace ArtZilla.Config {
+	public class JsonStreamSerializer : IStreamSerializer {
+		public JsonStreamSerializer()
+			=> _serializer = JsonSerializer.Create(
+			new JsonSerializerSettings {
+				Formatting = Formatting.Indented,
+			});
+
+		/// <inheritdoc />
+		public void Serialize(Stream stream, Type type, object obj) {
+			using var sr = new StreamWriter(stream);
+			using var jsonTextWriter = new JsonTextWriter(sr);
+			_serializer.Serialize(jsonTextWriter, obj, type);
+		}
+
+		/// <inheritdoc />
+		public object Deserialize(Stream stream, Type type) {
+			using var sr = new StreamReader(stream);
+			using var jsonTextReader = new JsonTextReader(sr);
+			return _serializer.Deserialize(jsonTextReader, type);
+		}
+
+		private readonly JsonSerializer _serializer;
+	}
+}
